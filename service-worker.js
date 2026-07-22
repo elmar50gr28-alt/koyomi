1f3d78dc587737397b693bd463318733a498d72e
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-07-22 11:59:44 +0900

    Clarify Bazi result summary

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'koyomi-foundation-20260722-14';
+const CACHE_VERSION = 'koyomi-foundation-20260722-15';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
