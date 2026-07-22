8d3cb70c5bf8a5cbe89af2d3ef2484b09f2ae6fc
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-07-22 15:06:01 +0900

    Add full numerology reading

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'koyomi-foundation-20260722-22';
+const CACHE_VERSION = 'koyomi-foundation-20260722-23';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
