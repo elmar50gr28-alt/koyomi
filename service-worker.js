0f7c1ff1881052498bc8a5898bd211512c0cb414
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-07-22 12:25:21 +0900

    Add readable astrology result summary (#57)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'koyomi-foundation-20260722-17';
+const CACHE_VERSION = 'koyomi-foundation-20260722-18';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
