767e39b58ddd896ebc05d332d8f324dbe22f76bd
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-07-22 20:03:46 +0900

    Fix home and reading entry actions (#81)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'koyomi-foundation-20260722-41';
+const CACHE_VERSION = 'koyomi-foundation-20260722-42';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
