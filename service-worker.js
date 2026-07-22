4267e80f92efa8eb70506749c715cbc1636684a0
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-07-22 15:30:00 +0900

    Add full timing reading

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'koyomi-foundation-20260722-26';
+const CACHE_VERSION = 'koyomi-foundation-20260722-27';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
