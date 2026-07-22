8283ab7cd7626c4f33d8c9e2df425ae6152514e6
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-07-22 19:18:04 +0900

    Make reading guidance directly actionable

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'koyomi-foundation-20260722-39';
+const CACHE_VERSION = 'koyomi-foundation-20260722-40';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
