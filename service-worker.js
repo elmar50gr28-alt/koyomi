c17d8da1158026bb967d5863b0f06766a1c23d1e
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-07-22 16:06:59 +0900

    Unify divination reading voice (#69)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'koyomi-foundation-20260722-29';
+const CACHE_VERSION = 'koyomi-foundation-20260722-30';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
