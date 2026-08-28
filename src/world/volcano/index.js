3276c287b64706edb93e02ee0bd582d256c86497
elmar50gr28-alt <elmar50gr28@gmail.com>
2026-08-28 21:20:01 +0900

    Migrate volcano observations to GVP ids

--- src/world/volcano/index.js
+++ src/world/volcano/index.js
@@ -5,3 +5,4 @@ export * from './release-gate.js';
 export * from './display-policy.js';
 export * from './alert-level.js';
 export * from './localization.js';
+export * from './observation-compat.js';
